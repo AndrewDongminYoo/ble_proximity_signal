528f750f4fb6cda705ab148646e64232f78d522b
Dongmin, Yu <ydm2790@gmail.com>
2026-02-10 21:51:39 +0900

    test: ✅ configure android example for patrol integration tests
    This commit prepares the Android example application for running Patrol integration tests.
    
    *   Updates Java and Kotlin compatibility to version 17.
    *   Integrates `PatrolJUnitRunner` as the test instrumentation runner.
    *   Enables `ANDROIDX_TEST_ORCHESTRATOR` for test execution.
    *   Adds `MainActivityTest.kt` to dynamically discover and execute Dart tests using Patrol's setup.
    *   Disables minification and resource shrinking for the debug build type to ensure test compatibility.
    *   Adds `androidx.test:orchestrator` dependency.

--- ble_proximity_signal/example/android/app/build.gradle.kts
+++ ble_proximity_signal/example/android/app/build.gradle.kts
@@ -1,9 +1,7 @@
-import java.io.FileInputStream
-import java.util.Properties
-
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -13,21 +11,36 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+    kotlin {
+        compilerOptions {
+            jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        }
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.andrew.signal.example"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        testInstrumentationRunner = "pl.leancode.patrol.PatrolJUnitRunner"
+        testInstrumentationRunnerArguments["clearPackageData"] = "true"
+    }
+
+    testOptions {
+        execution = "ANDROIDX_TEST_ORCHESTRATOR"
+    }
+
+    dependencies {
+        androidTestUtil("androidx.test:orchestrator:1.6.1")
     }
 
     buildTypes {
@@ -35,6 +48,10 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+        debug {
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
